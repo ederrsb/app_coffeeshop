524c09dca23179a56b6edc80dac41a52b5ecebd7
Eder <ederrsb@gmail.com>
2023-11-08 23:15:04 -0300

    Primeiro Commit

--- endpoints/venda.py
+++ endpoints/venda.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from conexao_db import Conexao
 
 venda_bp = Blueprint('venda', __name__)
@@ -21,6 +21,47 @@ def obter_vendas():
 def obter_venda(id_venda):
     query = 'SELECT * FROM venda WHERE id_venda = %s'
     resultado = conexao.execute_query(query, (id_venda,))
+
+    if resultado:
+        colunas = [column[0] for column in conexao.cursor.description]
+        venda = dict(zip(colunas, resultado[0]))
+        return jsonify(venda)
+    else:
+        return jsonify({'message': 'Venda não encontrada'}), 404
+
+@venda_bp.route('/vendas_item', methods=['GET'])
+def obter_venda_item():
+    tipo     = request.args.get('tipo') 
+    id_venda = request.args.get('id_venda')  
+
+    query = """
+            select case when vi.id_item is not null then 'Produto' else 'Assinatura' end as tipo, 
+                    v.id_venda,
+                    vi.item,
+                    coalesce(vi.id_item, vi.id_assinatura) as id_item,
+                    coalesce(i.descricao, a.descricao) as descricao,
+                    v.id_cliente,
+                    c.nome as nome_cliente,
+                    v.id_funcionario,
+                    f.nome,
+                    v.data,
+                    v.valor_total_venda,
+                    v.forma_pagamento,
+                    v.status_pagamento,
+                    vi.quantidade,
+                    vi.valor_unitario,
+                    vi.valor_desconto,
+                    vi.valor_total_item
+                from venda v
+                join venda_item vi on v.id_venda = vi.id_venda
+                join cliente c on v.id_cliente = c.id_cliente
+                left join funcionario f on v.id_funcionario = f.id_funcionario
+                left join item i on vi.id_item  = i.id_item
+                left join assinatura a on vi.id_assinatura = a.id_assinatura 
+                where case when vi.id_item is not null then 'Produto' else 'Assinatura' end = %s
+                  and v.id_venda = %s
+            """
+    resultado = conexao.execute_query(query, (tipo, id_venda))
 
     if resultado:
         colunas = [column[0] for column in conexao.cursor.description]
