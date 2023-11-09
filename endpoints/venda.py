cf5694ccd0f7b689e4ac2f5430dcc2ab5fdffca1
Eder <ederrsb@gmail.com>
2023-11-09 19:44:52 -0300

    Update venda.py
    Adicionado endpoint "vendas_item"

--- endpoints/venda.py
+++ endpoints/venda.py
@@ -28,44 +28,67 @@ def obter_venda(id_venda):
         return jsonify(venda)
     else:
         return jsonify({'message': 'Venda não encontrada'}), 404
-
+    
 @venda_bp.route('/vendas_item', methods=['GET'])
 def obter_venda_item():
-    tipo     = request.args.get('tipo') 
-    id_venda = request.args.get('id_venda')  
+    tipo = request.args.get('tipo')
+    id_venda = request.args.get('id_venda')
 
-    query = """
-            select case when vi.id_item is not null then 'Produto' else 'Assinatura' end as tipo, 
-                    v.id_venda,
-                    vi.item,
-                    coalesce(vi.id_item, vi.id_assinatura) as id_item,
-                    coalesce(i.descricao, a.descricao) as descricao,
-                    v.id_cliente,
-                    c.nome as nome_cliente,
-                    v.id_funcionario,
-                    f.nome,
-                    v.data,
-                    v.valor_total_venda,
-                    v.forma_pagamento,
-                    v.status_pagamento,
-                    vi.quantidade,
-                    vi.valor_unitario,
-                    vi.valor_desconto,
-                    vi.valor_total_item
-                from venda v
-                join venda_item vi on v.id_venda = vi.id_venda
-                join cliente c on v.id_cliente = c.id_cliente
-                left join funcionario f on v.id_funcionario = f.id_funcionario
-                left join item i on vi.id_item  = i.id_item
-                left join assinatura a on vi.id_assinatura = a.id_assinatura 
-                where case when vi.id_item is not null then 'Produto' else 'Assinatura' end = %s
-                  and v.id_venda = %s
-            """
-    resultado = conexao.execute_query(query, (tipo, id_venda))
+    # Inicializa uma lista de condições para a cláusula WHERE
+    conditions = []
+
+    # Adiciona a condição para o parâmetro 'tipo' se ele estiver presente
+    if tipo:
+        conditions.append("and case when vi.id_item is not null then 'Produto' else 'Assinatura' end = %s")
+
+    # Adiciona a condição para o parâmetro 'id_venda' se ele estiver presente
+    if id_venda:
+        conditions.append("and v.id_venda = %s")
+
+    # Constrói a cláusula WHERE combinando as condições com 'AND'
+    where_clause = "".join(conditions) if conditions else ""
+
+    # Constrói a consulta SQL com a cláusula WHERE
+    query = f"""
+        select case when vi.id_item is not null then 'Produto' else 'Assinatura' end as tipo, 
+               v.id_venda,
+               vi.item,
+               coalesce(vi.id_item, vi.id_assinatura) as id_item,
+               coalesce(i.descricao, a.descricao) as descricao,
+               v.id_cliente,
+               c.nome as nome_cliente,
+               v.id_funcionario,
+               f.nome,
+               v.data,
+               v.valor_total_venda,
+               v.forma_pagamento,
+               v.status_pagamento,
+               vi.quantidade,
+               vi.valor_unitario,
+               vi.valor_desconto,
+               vi.valor_total_item
+        from venda v
+        join venda_item vi on v.id_venda = vi.id_venda
+        join cliente c on v.id_cliente = c.id_cliente
+        left join funcionario f on v.id_funcionario = f.id_funcionario
+        left join item i on vi.id_item  = i.id_item
+        left join assinatura a on vi.id_assinatura = a.id_assinatura 
+        where 1=1 {where_clause}
+        order by 1, 2, 3
+    """
+
+    # Parâmetros a serem passados na consulta
+    params = [tipo, id_venda]
+
+    # Remove None da lista de parâmetros para os que não foram fornecidos
+    params = [param for param in params if param is not None]
+
+    # Executa a consulta SQL
+    resultado = conexao.execute_query(query, params)
 
     if resultado:
         colunas = [column[0] for column in conexao.cursor.description]
-        venda = dict(zip(colunas, resultado[0]))
-        return jsonify(venda)
+        vendas_itens = [dict(zip(colunas, vendas_item)) for vendas_item in resultado]
+        return jsonify(vendas_itens)
     else:
         return jsonify({'message': 'Venda não encontrada'}), 404
