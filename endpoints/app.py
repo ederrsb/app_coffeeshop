787e7877047cedf291e7d0dae517301d6bc98ae6
Eder <ederrsb@gmail.com>
2023-11-11 18:40:09 -0300

    Criado endpoints para "conta_estoque"

--- endpoints/app.py
+++ endpoints/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from venda import venda_bp
+from conta_estoque import conta_estoque_bp
 from assinatura import assinatura_bp
 from item import item_bp
 from logger import logger  # Importe o logger do logger.py
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.register_blueprint(venda_bp, url_prefix='')
 app.register_blueprint(assinatura_bp, url_prefix='')
 app.register_blueprint(item_bp, url_prefix='')
+app.register_blueprint(conta_estoque_bp, conta_estoque='')
 
 if __name__ == '__main__':
     app.run(port=5550, host='endpoints', debug=True)
---
+++ endpoints/conta_estoque.py
@@ -0,0 +1,82 @@
+from flask import Blueprint, jsonify, request
+from conexao_db import Conexao
+from logger import logger  # Certifique-se de importar o logger adequado
+
+conta_estoque_bp = Blueprint('conta_estoque', __name__)
+conexao = Conexao()
+
+@conta_estoque_bp.route('/contas_estoque', methods=['GET'])
+def obter_contas_estoque():
+    try:
+        query = 'SELECT * FROM conta_estoque'
+        resultado = conexao.execute_query(query)
+
+        if resultado:
+            colunas = [column[0] for column in conexao.cursor.description]
+            contas_estoque = [dict(zip(colunas, conta_estoque)) for conta_estoque in resultado]
+            return jsonify(contas_estoque)
+        else:
+            return jsonify([])
+    except Exception as e:
+        logger.error(f"Erro ao obter contas de estoque: {str(e)}")
+        return jsonify({'message': 'Erro ao obter contas de estoque'}), 500
+
+@conta_estoque_bp.route('/contas_estoque', methods=['POST'])
+def inserir_conta_estoque():
+    try:
+        dados_conta_estoque = request.get_json()
+
+        query = """
+            INSERT INTO db_coffeeshop.conta_estoque (descricao)
+            VALUES (%s)
+        """
+
+        params = (
+            dados_conta_estoque['descricao'],
+        )
+
+        conexao.execute_query(query, params)
+        conexao.connection.commit()
+
+        return jsonify({'message': 'Conta de estoque inserida com sucesso'}), 201
+    except Exception as e:
+        logger.error(f"Erro ao inserir conta de estoque: {str(e)}")
+        return jsonify({'message': 'Erro ao inserir conta de estoque'}), 500
+
+@conta_estoque_bp.route('/contas_estoque/<int:id_conta_estoque>', methods=['PUT'])
+def atualizar_conta_estoque(id_conta_estoque):
+    try:
+        dados_conta_estoque = request.get_json()
+
+        query = """
+            UPDATE db_coffeeshop.conta_estoque
+            SET descricao = %s
+            WHERE id_conta_estoque = %s
+        """
+
+        params = (
+            dados_conta_estoque['descricao'],
+            id_conta_estoque
+        )
+
+        conexao.execute_query(query, params)
+        conexao.connection.commit()
+
+        return jsonify({'message': 'Conta de estoque atualizada com sucesso'}), 200
+    except Exception as e:
+        logger.error(f"Erro ao atualizar conta de estoque: {str(e)}")
+        return jsonify({'message': 'Erro ao atualizar conta de estoque'}), 500
+
+@conta_estoque_bp.route('/contas_estoque/<int:id_conta_estoque>', methods=['DELETE'])
+def deletar_conta_estoque(id_conta_estoque):
+    try:
+        query = "DELETE FROM db_coffeeshop.conta_estoque WHERE id_conta_estoque = %s"
+        params = (id_conta_estoque,)
+
+        conexao.execute_query(query, params)
+        conexao.connection.commit()
+
+        return jsonify({'message': 'Conta de estoque deletada com sucesso'}), 200
+    except Exception as e:
+        logger.error(f"Erro ao deletar conta de estoque: {str(e)}")
+        return jsonify({'message': 'Erro ao deletar conta de estoque'}), 500
